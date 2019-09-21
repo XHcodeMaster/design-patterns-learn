2c69d64437aed08c90c07f39198c5671f457b5f1
DESKTOP-JJVMVV9\xh-pc <xinh001@cmft.com>
2019-09-21 13:55:44 +0800

    mvc

--- src/main/java/com/edu/mvc/dispatcher/Checker.java
+++ src/main/java/com/edu/mvc/dispatcher/Checker.java
@@ -2,22 +2,24 @@ package com.edu.mvc.dispatcher;
 
 /**
  * 策略的场景类
- *
  */
 public class Checker {
-//使用哪一个策略
-private IXmlValidate validate;
-//xml配置文件的路径
-String xmlPath;
-//构造函数传递
-public Checker(IXmlValidate _validate){
-this.validate = _validate;
-}
-public void setXmlPath(String _xmlPath){
-this.xmlPath = _xmlPath;
-}
-//检查
-public boolean check(){
-return validate.validate(xmlPath);
-}
+    //使用哪一个策略
+    private IXmlValidate validate;
+    //xml配置文件的路径
+    String xmlPath;
+
+    //构造函数传递
+    public Checker(IXmlValidate _validate) {
+        this.validate = _validate;
+    }
+
+    public void setXmlPath(String _xmlPath) {
+        this.xmlPath = _xmlPath;
+    }
+
+    //检查
+    public boolean check() {
+        return validate.validate(xmlPath);
+    }
 }
